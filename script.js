08a8e170161a635b8e827f89f13bcfce342f0f16
ugbeadie <ugbeadie3@gmail.com>
2022-03-04 20:32:42 +0100

    manage

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navUl = document.getElementsByClassName("navbar-links")[0]
-const navMenu = document.querySelector(".navbar");
+const navUl = document.getElementsByClassName("nav-items")[0]
+const navMenu = document.querySelector("nav");
 
 toggleButton.addEventListener('click', () => {
-    // navUl.classList.toggle('active');
+    navUl.classList.toggle('active');
     document.body.classList.toggle("cross");
     // document.body.classList.toggle("black");    
 });
@@ -18,23 +18,8 @@ toggleButton.addEventListener('click', () => {
 //     }
 // });
 
-// function hideUl() {
-//     document.body.classList.toggle("cross");
-//     navbarLinks.classList.toggle('active');
-//     document.body.classList.toggle("black");
-// }
-const btn = document.getElementsByClassName('t-btn');
-const slide = document.getElementById('slide');
-
-btn[0].onclick = function () {
-    slide.style.backgroundColur = 'translate(0px)';
-}
-btn[0].onclick = function () {
-    slide.style.backgroundColur = 'translate(-800px)';
-}
-btn[0].onclick = function () {
-    slide.style.backgroundColur = 'translate(-1600px)';
-}
-btn[0].onclick = function () {
-    slide.style.backgroundColur = 'translate(-2400px)';
+function hideUl() {
+    document.body.classList.toggle("cross");
+    navbarLinks.classList.toggle('active');
+    // document.body.classList.toggle("black");
 }
