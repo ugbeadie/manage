8b92168cddadb732dc6ead20e48a5ec660d398ce
ugbeadie <ugbeadie3@gmail.com>
2022-03-06 21:46:23 +0100

    manage

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ toggleButton.addEventListener('click', () => {
     // document.body.classList.toggle("black");    
 });
 
+function validation() {
+    var form = document.getElementById('form');
+    var email = document.getElementById('email').value;
+    var text = document.getElementById('text');
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add('valid');
+        form.classList.remove('invalid');
+        text.innerHTML = 'Your Email Address Is valid';
+        text.style.color = 'var(--palered)';
+    }
+    else {
+        form.classList.remove('valid');
+        form.classList.add('invalid');
+        text.innerHTML = 'Please Insert A Valid Email Address';
+        text.style.color = 'var(--brightred)';
+    }
+}
+
 // window.addEventListener('scroll',  function() {
 //     if (window.scrollY > 770) {
 //         navbar.style.position = 'fixed';
